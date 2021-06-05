06cff0f65df3287f4cd6278c4612e123977dab51
Nate Lundell <nlundell@smitty.lundellnet.com>
2021-06-05 14:45:32 -0500

    adding scripts and networkd-dispatcher examples

---
+++ networkd-dispatcher-examples/no-carrier.d/10-interface-process-down.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if ! /usr/local/sbin/netsys-base is_init; then
+    export GBL_SYS_IGNORE_DEVS_VAL=$(
+        while read -r vpnConf; do
+            tapName=$(cat $vpnConf | sed -r -ne 's/dev\s(tap.+)/\1/p')
+            echo $tapName
+        done < <(find /etc/openvpn/server/*.conf -type f)
+    )
+
+    /usr/local/sbin/netsys-base init
+fi
+
+/usr/local/sbin/netsys-dev inform-state carrier-lost dev:$IFACE try-config
---
+++ networkd-dispatcher-examples/routable.d/10-interface-process-up.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/sbin/netsys-dev --errors --debug inform-state routable dev:$IFACE try-config
